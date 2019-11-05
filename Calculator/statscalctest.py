161511da4c64b4bc8dad4b832beb7d6d111a45f4
Sneha Philip <snehaphilip989@gmail.com>
2019-11-05 17:35:53 -0500

    Feature: updated stats by Population median

--- Calculator/statscalctest.py
+++ Calculator/statscalctest.py
@@ -11,9 +11,13 @@ class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
-    def stat_mean_calc(self):
+    def test_stat_mean_calc(self):
         data = [1,2,3,4,5]
         self.assertEqual(self.calculator.populationmean(data), 3)
+
+    def test_stat_median_calc(self):
+        data = [1, 2, 3, 4, 5, 6]
+        self.assertEqual(self.calculator.popmedian(data), 3.5)
 
 
 if __name__ == '__main__':
