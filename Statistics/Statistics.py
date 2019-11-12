1f67f4d5fca0a943dd3474007377d367b6b37667
Sneha Philip <snehaphilip989@gmail.com>
2019-11-12 11:22:14 -0500

    Task: Defined Population Std dev Function

---
+++ Statistics/Statistics.py
@@ -0,0 +1,30 @@
+from PopulationMean import mean
+from Median import median
+from Mode import mode
+from Popstd import stddev
+from PopVar import Popvar
+class Statistics:
+    result = 0
+
+    def __init__(self):
+        pass
+
+    def populationmean(self, a):
+        self.result = mean(a)
+        return self.result
+
+    def popmedian(self, a):
+        self.result = median(a)
+        return self.result
+
+    def popmode(self, a):
+        self.result = mode(a)
+        return self.result
+
+    def popstddev(self):
+        self.result = stddev()
+        return self.result
+
+    def popvariance(self, a):
+        self.result = Popvar(a)
+        return self.result
